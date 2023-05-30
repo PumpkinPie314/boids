c4d5ef9ad116eb3221e051306d3140c2608b88f6
Pumpkin <elijahgarland33@gmail.com>
2023-05-30 10:27:42 -0700

    yup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use bevy::{prelude::*, render::render_resource::PrimitiveTopology, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, render::{mesh::Indices, render_resource::PrimitiveTopology}};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_startup_system(spawn_boids)
         .run();
 }
+
+fn create_boid_mesh() -> Mesh{
+
+    let vertices = [
+        ([0.0, 1.0, 0.0]),
+        ([-1.0, -1.0, 0.0]),
+        ([1.0, -1.0, 0.0]),
+    ];
+
+    let indices = Indices::U32(vec![0, 2, 1, 0, 3, 2]);
+
+    let positions: Vec<_> = vertices.iter().map(|p| *p).collect();
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(indices));
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh
+}
+
 
 
 fn setup(
@@ -15,9 +35,9 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)),
-        material: materials.add(ColorMaterial::from(Color::WHITE)),
+        mesh: meshes.add(create_boid_mesh()).into(),
+        transform: Transform::default().with_scale(Vec3::splat(50.)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
         ..default()
     });
 }
