a127659c8f29dd8f7adb072edcbd4fcb6cfee5a8
Pumpkin <elijahgarland33@gmail.com>
2023-06-07 05:26:41 -0700

    implemented min speed

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use std::f32::consts::{PI, TAU};
 
-use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology}, sprite::MaterialMesh2dBundle, transform::commands};
+use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology}, sprite::MaterialMesh2dBundle, transform::{commands, self}};
 use rand::Rng;
 
 const BOID_COLOR:Color = Color::PURPLE;
-const SPEED:f32 = 100.0;
 const BOID_COUNT:i32 = 10;
-
+const BOID_MIN_SPEED: f32 = 10.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_boids_system)
-        .add_system(update_posistions_system)
+        .add_systems((
+            move_forward_system,
+            //speed_up_slow_boids_system,
+
+        ))
+        // .add_system(turn_system)
         .run();
 }
 
@@ -23,9 +27,9 @@ pub fn setup_camera(mut commands: Commands){
 fn create_boid_mesh() -> Mesh{
 
     let vertices = [
-        ([-1.0, 1.0, 0.0]),
+        ([0.0, 1.0, 0.0]),
         ([-1.0, -1.0, 0.0]),
-        ([1.0, 0.0, 0.0]),
+        ([1.0, -1.0, 0.0]),
     ];
 
     let indices = Indices::U32(vec![0, 2, 1, 0, 3, 2]);
@@ -40,35 +44,41 @@ fn create_boid_mesh() -> Mesh{
 
 #[derive(Component)]
 struct Boid {}
-
+#[derive(Component)]
+struct Velocity(Vec2);
 fn spawn_boids_system(
-    commands: Commands,
-    meshes: ResMut<Assets<Mesh>>,
-    materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    for _ in 0..BOID_COUNT {
     spawn_boid(
         0.0,
         0.0,
-        PI/2.0,
-        commands,
-        meshes,
-        materials,
+        Vec2 {
+            x: rand::thread_rng().gen_range(-10.0..10.0),
+            y: rand::thread_rng().gen_range(-10.0..10.0),
+        },
+        &mut commands,
+        &mut meshes,
+        &mut materials,
     )
+    }
 }
 
 fn spawn_boid(
     x: f32,
     y: f32,
-    angle: f32,
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    velocity: Vec2,
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn((
         MaterialMesh2dBundle {
             transform: Transform{
                 translation : Vec3 { x, y, z: 0.0 },
-                rotation : Quat::from_rotation_z(angle),
+                rotation : Quat::from_rotation_z(-velocity.x.atan2(velocity.y)),
                 scale : Vec3::splat(20.)
             },
             mesh: meshes.add(create_boid_mesh()).into(),
@@ -76,19 +86,26 @@ fn spawn_boid(
             ..default()
         },
         Boid {},
+        Velocity(velocity),
     ));
 }
 
 
-fn update_posistions_system(
-    mut query: Query<&mut Transform, With<Boid>>,
+fn move_forward_system(
+    mut query: Query<(&mut Transform, &Velocity)>,
     timer: Res<Time>
 ) {
-    for mut boid in query.iter_mut() {
-        let (_, angle) = boid.rotation.to_axis_angle();
-        boid.translation.x = boid.translation.x + angle.cos()*SPEED*timer.delta_seconds();
-        boid.translation.y = boid.translation.y + angle.sin()*SPEED*timer.delta_seconds();
-        boid.rotate_z(0.01);
-        println!("{}", angle/TAU)
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.0.x * timer.delta_seconds();
+        transform.translation.y += velocity.0.y * timer.delta_seconds();
+    }
+}
+fn speed_up_slow_boids_system(
+    mut query: Query<&mut Velocity>
+) {
+    for mut velocity in query.iter_mut() {
+        if velocity.0.length() < BOID_MIN_SPEED {
+            velocity.0 = velocity.0.normalize() * BOID_MIN_SPEED;
+        }
     }
 }
