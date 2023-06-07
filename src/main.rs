332ad8c94564dabfec9aa454b01311bbe1d25d35
Pumpkin <elijahgarland33@gmail.com>
2023-06-06 19:38:57 -0700

    spining triangle, angle and speed. bad bad wrong

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use std::f32::consts::{PI, TAU};
+
 use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology}, sprite::MaterialMesh2dBundle, transform::commands};
 use rand::Rng;
 
 const BOID_COLOR:Color = Color::PURPLE;
-const SPEED:f32 = 10.0;
+const SPEED:f32 = 100.0;
+const BOID_COUNT:i32 = 10;
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_camera)
-        .add_startup_system(spawn_boid)
+        .add_startup_system(spawn_boids_system)
         .add_system(update_posistions_system)
         .run();
 }
@@ -37,7 +41,25 @@ fn create_boid_mesh() -> Mesh{
 #[derive(Component)]
 struct Boid {}
 
+fn spawn_boids_system(
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+) {
+    spawn_boid(
+        0.0,
+        0.0,
+        PI/2.0,
+        commands,
+        meshes,
+        materials,
+    )
+}
+
 fn spawn_boid(
+    x: f32,
+    y: f32,
+    angle: f32,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -45,9 +67,9 @@ fn spawn_boid(
     commands.spawn((
         MaterialMesh2dBundle {
             transform: Transform{
-                translation : Vec3 { x: 0.0, y: 1.0, z: 0.0 },
-                rotation : Quat::from_rotation_z(0.0),
-                scale : Vec3::splat(50.)
+                translation : Vec3 { x, y, z: 0.0 },
+                rotation : Quat::from_rotation_z(angle),
+                scale : Vec3::splat(20.)
             },
             mesh: meshes.add(create_boid_mesh()).into(),
             material: materials.add(ColorMaterial::from(BOID_COLOR)),
@@ -60,9 +82,13 @@ fn spawn_boid(
 
 fn update_posistions_system(
     mut query: Query<&mut Transform, With<Boid>>,
+    timer: Res<Time>
 ) {
     for mut boid in query.iter_mut() {
-        boid.translation.x = boid.translation.x + 1.0;
-        println!("{:?}",boid)
+        let (_, angle) = boid.rotation.to_axis_angle();
+        boid.translation.x = boid.translation.x + angle.cos()*SPEED*timer.delta_seconds();
+        boid.translation.y = boid.translation.y + angle.sin()*SPEED*timer.delta_seconds();
+        boid.rotate_z(0.01);
+        println!("{}", angle/TAU)
     }
 }
