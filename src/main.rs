e1a52d998e526a3dd358b6b6949b6d63eaabe83f
Pumpkin <elijahgarland33@gmail.com>
2023-06-11 19:07:44 -0700

    reboid: started again

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,88 @@
-use std::f32::consts::{PI, TAU};
+use std::ops::Mul;
 
-use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology, view::window}, sprite::MaterialMesh2dBundle, window::PrimaryWindow, ecs::query};
+use bevy::prelude::*;
+use bevy::render::{render_resource::PrimitiveTopology, mesh::Indices};
 use rand::Rng;
-
-const BOID_COLOR:Color = Color::PURPLE;
-const BOID_COUNT:i32 = 10;
-const BOID_MIN_SPEED: f32 = 20.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup_camera)
-        .add_startup_system(spawn_boids_system)
-        .add_systems((
-            move_forward_system,
-            speed_up_slow_boids_system,
-            wrap_around_system,
-            alignment,
-        ))
-        // .add_system(turn_system)
-        .run();
+    .add_plugins(DefaultPlugins)
+    .add_event::<UpdateVelocityEvent>()
+    .add_startup_system(setup_camera)
+    .add_startup_system(spawn_boids_system)
+    .add_system(movement_system)
+    .add_system(wrap_around_system)
+    .add_system(flocking_system)
+    .add_system(update_velocitys)
+    .run();
 }
-
-pub fn setup_camera(mut commands: Commands){
+fn setup_camera(
+    mut commands: Commands
+) {
     commands.spawn(Camera2dBundle::default());
 }
-fn create_boid_mesh() -> Mesh{
 
-    let vertices = [
-        ([0.0, 1.0, 0.0]),
-        ([-1.0, -1.0, 0.0]),
-        ([1.0, -1.0, 0.0]),
-    ];
-
-    let indices = Indices::U32(vec![0, 2, 1, 0, 3, 2]);
-
-    let positions: Vec<_> = vertices.iter().map(|p| *p).collect();
-
+fn create_triagle_mesh() -> Mesh {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_indices(Some(indices));
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vec![
+        [0.0, 2.0, 0.0],
+        [-1.0, -1.0, 0.0],
+        [1.0, -1.0, 0.0],
+    ]);
+    mesh.set_indices(Some(Indices::U32(vec![0,1,2])));
     mesh
 }
 
 #[derive(Component)]
-struct SpecialTemp {}
+struct Velocity(Vec3);
 
-#[derive(Component)]
-struct Boid {}
-#[derive(Component)]
-struct Velocity(Vec2);
+struct UpdateVelocityEvent(Entity, Vec3, f32);
+
 fn spawn_boids_system(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    for _ in 0..BOID_COUNT {
-        spawn_boid(
-            0.0,
-            0.0,
-            Vec2 {
-                x: rand::thread_rng().gen_range((-2.0*BOID_MIN_SPEED)..(2.0*BOID_MIN_SPEED)),
-                y: rand::thread_rng().gen_range((-2.0*BOID_MIN_SPEED)..(2.0*BOID_MIN_SPEED)),
-            },
-            &mut commands,
-            &mut meshes,
-            &mut materials,
-        );
-        let svelocity = Vec2 { x: 1.0, y: 0.0 };
+    window_query: Query<&Window>,
+){
+    let mut random_thread = rand::thread_rng();
+    let window = window_query.get_single().unwrap();
+    for _ in 0..=50 {
         commands.spawn((
-            MaterialMesh2dBundle {
-                transform: Transform{
-                    translation : Vec3 { x: 0.0, y: 0.0, z: 0.0 },
-                    rotation : Quat::from_rotation_z(-svelocity.x.atan2(svelocity.y)),
-                    scale : Vec3::splat(20.)
+            ColorMesh2dBundle {
+                material: materials.add(ColorMaterial::from(Color::PURPLE)), 
+                mesh: meshes.add(create_triagle_mesh()).into(),
+                transform: Transform {
+                    scale: Vec3::splat(10.0),
+                    translation: Vec3 {
+                        x: random_thread.gen_range(-window.width()..window.width()),
+                        y: random_thread.gen_range(-window.height()..window.height()),
+                        z: 0.0,
+                    },
+                    ..default()
                 },
-                mesh: meshes.add(create_boid_mesh()).into(),
-                material: materials.add(ColorMaterial::from(Color::RED)),
                 ..default()
             },
-            Boid {},
-            SpecialTemp{},
-            Velocity(svelocity),
+            Velocity(Vec3{
+                x: random_thread.gen_range(-1.0..1.0),
+                y: random_thread.gen_range(-1.0..1.0),
+                z: 0.0,
+            }.normalize().mul(Vec3::splat(50.0)))
         ));
     }
 }
 
-fn spawn_boid(
-    x: f32,
-    y: f32,
-    velocity: Vec2,
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn((
-        MaterialMesh2dBundle {
-            transform: Transform{
-                translation : Vec3 { x, y, z: 0.0 },
-                rotation : Quat::from_rotation_z(-velocity.x.atan2(velocity.y)),
-                scale : Vec3::splat(20.)
-            },
-            mesh: meshes.add(create_boid_mesh()).into(),
-            material: materials.add(ColorMaterial::from(BOID_COLOR)),
-            ..default()
-        },
-        Boid {},
-        Velocity(velocity),
-    ));
-}
-
-
-fn move_forward_system(
-    mut query: Query<(&mut Transform, &Velocity)>,
-    timer: Res<Time>
+fn movement_system(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &Velocity)>
 ) {
     for (mut transform, velocity) in query.iter_mut() {
-        transform.translation.x += velocity.0.x * timer.delta_seconds();
-        transform.translation.y += velocity.0.y * timer.delta_seconds();
+        transform.translation += velocity.0 * time.delta_seconds();
+        transform.rotation = Quat::from_rotation_z((-velocity.0.x).atan2(velocity.0.y));
     }
 }
-fn speed_up_slow_boids_system(
-    mut query: Query<&mut Velocity>
-) {
-    for mut velocity in query.iter_mut() {
-        if velocity.0.length() < BOID_MIN_SPEED {
-            velocity.0 = velocity.0.normalize() * BOID_MIN_SPEED;
-        }
-    }
-}
+
 fn wrap_around_system(
-    window_query: Query<&Window, With<PrimaryWindow>>,
+    window_query: Query<&Window>,
     mut query: Query<&mut Transform>
 ) {
     let window_entity = window_query.get_single().unwrap();
@@ -152,13 +104,37 @@ fn wrap_around_system(
     }
 }
 
-// fn alignment(
-//     mut query: Query<(&mut Velocity, &Transform)>
-// ) {
-//     for (mut my_velocity, my_position) in query.iter_mut() {
-//         query.iter().filter(|other_boid|{
-//             let other_position = other_boid.1.translation;
-//             false
-//         });
-//     }
-// }
+fn flocking_system(
+    mut update_velocity_event : EventWriter<UpdateVelocityEvent>,
+    query: Query<(Entity, &Transform, &Velocity)>,
+){
+    for (entity_me, transform_me, velocity_me) in query.iter() {
+        update_velocity_event.send(UpdateVelocityEvent(
+            entity_me,
+            Vec3 { x: 0.0, y: -1.0, z: 0.0},
+            1.0
+        ))
+        // let position_me: Vec3 = transform_me.translation;
+        // let mut nabor_count: i32 = 0;
+        // let mut average_nabor_velocity: Vec3 = Vec3::ZERO;
+        // for (entity_other, transform_other, velocity_other) in query.iter() {
+        //     let position_other = transform_other.translation;
+        //     if entity_me == entity_other {continue;};
+        //     if position_me.distance(position_other) < 40.0 {
+        //         let away_from_other: Vec3{
+                    
+        //         };
+        //     };
+        // };
+    }
+}
+
+fn update_velocitys(
+    mut update_velocity_event : EventReader<UpdateVelocityEvent>,
+    mut query: Query<&mut Velocity>,
+){
+    for UpdateVelocityEvent(entity, direction, force) in update_velocity_event.iter() {
+        let mut velocity = query.get_mut(*entity).unwrap();
+        velocity.0 += *direction * Vec3::splat(*force)
+    };
+}
