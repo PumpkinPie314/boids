e410a504a76c3fab2774cee537baed7e48db807b
Pumpkin <elijahgarland33@gmail.com>
2023-06-12 08:33:16 -0700

    done :)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use bevy::prelude::*;
 use bevy::render::{render_resource::PrimitiveTopology, mesh::Indices};
 use rand::Rng;
 
+const BOID_SPEED: f32 = 150.0;
+const BOID_VISION: f32 = 80.0;
+const BOID_COLLISION_RADIUS: f32 = 30.0;
+const ALIGNMENT_FORCE: f32 = 0.2;
+const COHESION_FORCE: f32 = 0.2;
+const SEPERAION_FORCE: f32 = 0.3;
+
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
@@ -61,7 +69,7 @@ fn spawn_boids_system(
                 x: random_thread.gen_range(-1.0..1.0),
                 y: random_thread.gen_range(-1.0..1.0),
                 z: 0.0,
-            }.normalize() * Vec3::splat(100.0))
+            }.normalize() * Vec3::splat(BOID_SPEED))
         ));
     }
 }
@@ -105,51 +113,49 @@ struct Intruction{
     factor: f32,
 }
 
-const VISION :f32 = 80.0;
-const SIZE :f32 = 20.0;
-
 fn flocking_system(
     mut query: Query<(Entity, &Transform, &mut Velocity)>
 ){
     let mut queue: Vec<Intruction> = vec![];
     for (boid_a, transform_a, _velocity_a) in query.iter() {
         let position_a = transform_a.translation;
-        let mut nabor_count: u32 = 0;
-
-        let mut average_velocity = Vec3::ZERO;//alignment
-        let mut average_nabor_position = Vec3::ZERO;//alignment
-
-        for (boid_b, transform_b, velocity_b) in query.iter() {
+        let mut neighbour_count: u32 = 0;
+        let mut average_velocity = Vec3::ZERO;
+        let mut average_neighbour_position = Vec3::ZERO;
+	    for (boid_b, transform_b, velocity_b) in query.iter() {
             if boid_a == boid_b {continue}; 
             let position_b = transform_b.translation;
-            if position_a.distance(position_b) > VISION {continue};
-            if position_a.distance(position_b) < SIZE {
-                let away_direction = position_a - position_b;
-                queue.push(Intruction {
-                    boid: boid_a,
-                    direction: away_direction,
-                    factor: 0.3,
-                })
+            if position_a.distance(position_b) < BOID_VISION {
+                neighbour_count += 1;
+                average_velocity += velocity_b.0;
+                average_neighbour_position += position_b
             };
-            nabor_count += 1;
-            average_velocity += velocity_b.0;
-            average_nabor_position += position_b
-        }
-        if nabor_count == 0 {continue};
-        average_velocity /= Vec3::splat(nabor_count as f32);
-        queue.push(Intruction {
+            if position_a.distance(position_b) < BOID_COLLISION_RADIUS {
+                let away_vector = position_a - position_b;
+                let seperation = Intruction {
+                    boid: boid_a,
+                    direction: away_vector,
+                    factor: SEPERAION_FORCE,
+                };
+                queue.push(seperation);
+            }
+	    };
+        if neighbour_count == 0 {continue};
+        average_velocity /= Vec3::splat(neighbour_count as f32);
+        average_neighbour_position /= Vec3::splat(neighbour_count as f32);
+        let towards_neighbours = average_neighbour_position - position_a;
+        let alignment = Intruction {
             boid: boid_a,
             direction: average_velocity,
-            factor: 0.2,
-        });
-        average_nabor_position /= Vec3::splat(nabor_count as f32);
-        let tward_nabors = average_nabor_position - position_a;
-        queue.push(Intruction {
+            factor: ALIGNMENT_FORCE,
+        };
+        queue.push(alignment);
+        let cohesion = Intruction {
             boid: boid_a,
-            direction: tward_nabors,
-            factor: 0.2,
-        });
-
+            direction: towards_neighbours,
+            factor: COHESION_FORCE,
+        };
+        queue.push(cohesion);
     };
     for (boid, _transform, mut velocity) in query.iter_mut() {
         for instruction in queue.iter() {
